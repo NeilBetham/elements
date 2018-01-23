ababc3ee6f518ae0eb5b3395ef5e7fd0dfcc0183
Neil Betham <nbetham@me.com>
2018-01-23 23:09:02 +0000

    Update some register settings

--- main.go
+++ main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+  "os"
+  "time"
+  "log"
+  "periph.io/x/periph/host"
+  "periph.io/x/periph/conn/gpio"
+  "periph.io/x/periph/conn/gpio/gpioreg"
+  "github.com/NeilBetham/elements/radios"
+)
+
+func main() {
+  if _, err := host.Init(); err != nil {
+		os.Exit(1)
+	}
+
+  resetPin := gpioreg.ByName("GPIO4")
+
+  resetPin.Out(gpio.High)
+  time.Sleep(5 * time.Millisecond)
+  resetPin.Out(gpio.Low)
+  time.Sleep(50 * time.Millisecond)
+
+  r, _ := radios.NewRFM69("/dev/spidev0.0")
+
+  r.SetFreq(902355835)
+  r.StartRead()
+
+  for{
+    time.Sleep(3 * time.Second)
+    log.Printf("Dumping Registers...")
+    r.DumpRegs()
+    r.StartRSSI()
+  }
+
+  os.Exit(0)
+}
--- radios/rfm69.go
+++ radios/rfm69.go
@@ -1,41 +1,42 @@
 package radios
 
 import (
-  "time"
-  "fmt"
   "log"
   "reflect"
+  "sort"
   "periph.io/x/periph/conn/spi"
   "periph.io/x/periph/conn/spi/spireg"
 )
 
-const(
-  opModeReg = 0x01
-  dataModulationReg = 0x02
-  bitRateMsbReg = 0x03
-  bitRateLsbReg = 0x04
-  freqDevMsbReg = 0x05
-  freqDevLsbReg = 0x06
-  carrierFreqMsbReg = 0x07
-  carrierFreqMidReg = 0x08
-  carrierFreqLsbReg = 0x09
-  rxBwFiltContReg = 0x019
-  afcBwFiltContReg = 0x1a
-  afcFeiContStatReg = 0x1e
-  rssiConfReg = 0x23
-  dioMappingReg = 0x25
-  irqFlags2Reg = 0x28
-  rssiThreshReg = 0x29
-  preambleMsbReg = 0x2c
-  preambleLsbReg = 0x2d
-  syncConfigReg = 0x2e
-  syncVal1Reg = 0x2f
-  syncval2Reg = 0x30
-  packetConfig1Reg = 0x37
-  payloadLengthReg = 0x38
-  packetConfig2Reg = 0x3d
-  testDagcReg = 0x6f
-)
+var regAddrs = map[string]uint8{
+  "opMode": 0x01,
+  "dataModulation": 0x02,
+  "bitRateMsb": 0x03,
+  "bitRateLsb": 0x04,
+  "freqDevMsb": 0x05,
+  "freqDevLsb": 0x06,
+  "carrierFreqMsb": 0x07,
+  "carrierFreqMid": 0x08,
+  "carrierFreqLsb": 0x09,
+  "rxBwFiltCont": 0x19,
+  "afcBwFiltCont": 0x1a,
+  "afcFeiContStat": 0x1e,
+  "rssiConf": 0x23,
+  "rssiValue": 0x24,
+  "dioMapping": 0x25,
+  "irqFlags1": 0x27,
+  "irqFlags2": 0x28,
+  "rssiThresh": 0x29,
+  "preambleMsb": 0x2c,
+  "preambleLsb": 0x2d,
+  "syncConfig": 0x2e,
+  "syncVal1": 0x2f,
+  "syncval2": 0x30,
+  "packetConfig1": 0x37,
+  "payloadLength": 0x38,
+  "packetConfig2": 0x3d,
+  "testDagc": 0x6f,
+}
 
 
 type rfm69Regs struct {
@@ -75,9 +76,9 @@ func newRFM69Regs() (r rfm69Regs){
   r.bitRateMsb = 0x06
   r.bitRateLsb = 0x83
 
-  // 4.8 kHz RX freq deviation
+  // 9.9 kHz RX freq deviation
   r.freqDevMsb = 0x00
-  r.freqDevLsb = 0x4e
+  r.freqDevLsb = 0xa1
 
   // RX Bandwidth
   // DCC Freq = 2, RX BW Mant = 20, RX BW Mant = 4
@@ -100,7 +101,7 @@ func newRFM69Regs() (r rfm69Regs){
   r.irqFlags2 = (1 << 4)
 
   // RSSI Threshold
-  r.rssiThresh = 170
+  r.rssiThresh = 228
 
   // Preambles
   r.preambleMsb = 0
@@ -108,7 +109,7 @@ func newRFM69Regs() (r rfm69Regs){
 
   // Sync Config
   // 2 sync bytes, allow two errors in sync bytes
-  r.syncConfig = (2 << 3) | (2 << 0)
+  r.syncConfig = (1 << 7) | (1 << 3) | (2 << 0)
 
   // Sync Values
   r.syncVal1 = 0xcb
@@ -137,9 +138,9 @@ type RFM69 struct {
 }
 
 // NewRFM69 sets up a new RFM69 class
-func NewRFM69(mosi int, miso int, sclk int, cs int, port string) (r RFM69, err error) {
+func NewRFM69(port string) (r RFM69, err error) {
   p, openErr := spireg.Open(port)
-  if openErr {
+  if openErr != nil {
     log.Printf("error: open spi port failed")
     err = openErr
     return
@@ -147,29 +148,95 @@ func NewRFM69(mosi int, miso int, sclk int, cs int, port string) (r RFM69, err e
 
   r.port = p
   r.freq = 915000000
-  r.recv_bytes = make([]byte)
+  r.recvBytes = make([]byte, 1)
   r.config = newRFM69Regs()
 
-  r.init()
+  err = r.init()
+  return
+}
+
+// SetFreq Sets the carrier freq of the RFM69
+func (r *RFM69) SetFreq(freq uint32) (err error){
+  r.config.carrierFreqLsb = uint8(freq / 61)
+  r.config.carrierFreqMid = uint8((freq / 61) >> 8)
+  r.config.carrierFreqMsb = uint8((freq / 61) >> 16)
+  err = r.syncRegs()
+  return
+}
+
+// StartRead puts the receiver in the RX mode and enables the interrupt
+func (r *RFM69) StartRead() (err error){
+  r.config.opMode = 0x04 // Put the chip into RX mode
+  r.writeReg(regAddrs["opMode"], r.config.opMode)
+  return
+}
+
+// DumpRegs dumps the current register settings in the RFM69
+func (r *RFM69) DumpRegs() (err error){
+  reverseMap := make(map[uint8]string)
+  for name, addr := range regAddrs {
+    reverseMap[addr] = name
+  }
+
+  var keys []int
+  for addr, _ := range reverseMap {
+     keys = append(keys, int(addr))
+  }
+  sort.Ints(keys)
+
+  for _, addr := range keys {
+    val, _ := r.readReg(uint8(addr))
+    log.Printf("%30s | 0x%.2x | %.8b\n", reverseMap[uint8(addr)], val, val)
+  }
   return
 }
 
 func (r *RFM69) init() (err error){
   // Setup the SPI connection with 5MHz baud, CPOL=0, CPHA=0, and 8 bit bytes
-  r.Connect(5000000, spi.Mode0, 8)
+  conn, err := r.port.Connect(5000000, spi.Mode0, 8)
+  if err != nil {
+    return err
+  }
+  r.conn = conn
 
+  r.syncRegs()
+  return
 }
 
-func (r rfm69Regs) syncRegs(regs RFM69Regs) (err error) {
-  for i := 0; i < r.Type().NumField(); i++ {
-
+func (r *RFM69) syncRegs() (err error) {
+  regValRef := reflect.ValueOf(r.config)
+  regTypeRef := reflect.TypeOf(r.config)
+  for i := 0; i < regValRef.NumField(); i++ {
+    regValue := uint8(regValRef.Field(i).Uint())
+    regAddr := regAddrs[regTypeRef.Field(i).Name]
+    err = r.writeReg(regAddr, regValue)
+    if err != nil {
+      break
+    }
   }
+  return
 }
 
 func (r *RFM69) writeReg(addr, val uint8) (err error){
-
+  bytesToSend := []byte{addr | 0x80, val}
+  bytesReceived := make([]byte, len(bytesToSend))
+  if err = r.conn.Tx(bytesToSend, bytesReceived); err != nil{
+    return err
+  }
+  return
 }
 
 func (r *RFM69) readReg(addr uint8) (b byte, err error){
+  bytesToSend := []byte{addr & 0x7f, 0x00}
+  bytesReceived := make([]byte, len(bytesToSend))
+  if err = r.conn.Tx(bytesToSend, bytesReceived); err != nil{
+    return 0x00, err
+  }
+  return bytesReceived[1], nil
+}
 
+// StartRSSI starts an RSSI reading
+func (r *RFM69) StartRSSI() (err error){
+  err = r.writeReg(regAddrs["rssiConf"], 0x01)
+  return
 }
